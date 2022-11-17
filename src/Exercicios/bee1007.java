6f62571fcd8e4ad51ac74e7cb07f654e60bf86b0
João Vitor <joaovitor8832@gmail.com>
2022-11-17 12:56:01 -0300

    Bee 1006

---
+++ src/Exercicios/bee1007.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1007 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int A,B,C,D;
+        A = s.nextInt();
+        B = s.nextInt();
+        C = s.nextInt();
+        D = s.nextInt();
+        int DIFERENCA = (A*B-C*D);
+        System.out.println("DIFERENCA = "+ DIFERENCA);
+    }
+}
