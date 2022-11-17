61861e35d213e82d6ef0968e653ca2021b792d99
João Vitor <joaovitor8832@gmail.com>
2022-11-17 14:42:17 -0300

    Bee 1012

---
+++ src/Exercicios/bee1012.java
@@ -0,0 +1,25 @@
+package Exercicios;
+import java.util.Scanner;
+public class bee1012 {
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo, pi;
+        pi = 3.14159;
+        A = s.nextDouble();
+        B = s.nextDouble();
+        C = s.nextDouble();
+
+        triangulo = (A*C)/2;
+        circulo = pi*Math.pow(C,2);
+        trapezio = ((A+B)*C)/2;
+        quadrado = Math.pow(B,2);
+        retangulo = A*B;
+
+        System.out.printf("TRIANGULO: %.3f\n",triangulo);
+        System.out.printf("CIRCULO: %.3f\n",circulo);
+        System.out.printf("TRAPEZIO: %.3f\n",trapezio);
+        System.out.printf("QUADRADO: %.3f\n",quadrado);
+        System.out.printf("RETANGULO: %.3f\n",retangulo);
+    }
+}
