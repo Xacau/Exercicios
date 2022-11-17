980a97530b7273f4cbed638620f725df45b4daa0
João Vitor <joaovitor8832@gmail.com>
2022-11-17 13:03:36 -0300

    Bee 1008

---
+++ src/Exercicios/bee1008.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1008 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int numFunc = s.nextInt();
+        int horas = s.nextInt();
+        double salarioHora = s.nextDouble();
+        double salario = salarioHora * horas;
+        System.out.println("NUMBER = " + numFunc);
+        System.out.printf("SALARY = U$ %.2f\n",salario);
+    }
+}
