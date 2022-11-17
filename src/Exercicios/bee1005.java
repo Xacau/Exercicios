800ccff11d07ef767a6eb812b875a2fa40fa6a78
João Vitor <joaovitor8832@gmail.com>
2022-11-17 12:43:42 -0300

    Bee 1005

---
+++ src/Exercicios/bee1005.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1005 {
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        double A = s.nextDouble();
+        double B = s.nextDouble();
+        double media = ((A*3.5)+(B*7.5))/11;
+        System.out.printf("MEDIA = %.5f\n",media);
+    }
+}
