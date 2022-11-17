55297fbcf9be67007d0340766d955f15565ea649
João Vitor <joaovitor8832@gmail.com>
2022-11-17 12:31:34 -0300

    Bee 1002

---
+++ src/Exercicios/bee1002.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1002 {
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        double pi = 3.14159;
+        double raio = s.nextDouble();
+        double area = pi*Math.pow(raio,2);
+        System.out.printf("A=%.4f\n",area);
+    }
+}
