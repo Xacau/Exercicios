41b187d670e9ba86cc90e5c2ceae3ac69be21d34
João Vitor <joaovitor8832@gmail.com>
2022-11-17 12:37:35 -0300

    Bee 1004

---
+++ src/Exercicios/bee1004.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1004 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int A = s.nextInt();
+        int B = s.nextInt();
+        int PROD = A*B;
+        System.out.println("PROD = " + PROD);
+    }
+}
