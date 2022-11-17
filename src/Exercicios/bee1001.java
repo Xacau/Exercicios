2dec02d27340620b7665e5771099a4cfef1e3d48
João Vitor <joaovitor8832@gmail.com>
2022-11-17 12:18:10 -0300

    Bee 1001

--- src/Exercicios/bee1001.java
+++ src/Exercicios/bee1001.java
@@ -7,8 +7,8 @@ public class bee1001 {
     public static void main(String[] args){
         int A = s.nextInt();
         int B = s.nextInt();
-        int C = 0;
-        int X = A + B + C;
+        int D = 0;
+        int X = A + B + D;
         System.out.println("X = " + X);
     }
 }
