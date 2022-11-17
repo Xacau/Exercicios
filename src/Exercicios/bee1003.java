99ca912c8d456666fc24e673a3b8fa1c2057f169
João Vitor <joaovitor8832@gmail.com>
2022-11-17 12:34:27 -0300

    Bee 1003

---
+++ src/Exercicios/bee1003.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1003 {
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int A = s.nextInt();
+        int B = s.nextInt();
+        int SOMA = A + B;
+        System.out.println("SOMA = " + SOMA);
+    }
+}
