a6411b7605638c2debe30f1c955c6bbd7c32d8cf
João Vitor <joaovitor8832@gmail.com>
2022-11-17 14:23:45 -0300

    Bee 1010

---
+++ src/Exercicios/bee1011.java
@@ -0,0 +1,12 @@
+package Exercicios;
+import java.util.Scanner;
+public class bee1011 {
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        double pi = 3.14159;
+        int R = s.nextInt();
+        double volume = (4.0/3)*pi*Math.pow(R,3);
+        System.out.printf("VOLUME = %.3f\n", volume);
+    }
+}
