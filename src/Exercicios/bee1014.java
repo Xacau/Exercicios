b9c855c369785411881d3331b6cc51c7f2a406b6
João Vitor <joaovitor8832@gmail.com>
2022-11-18 00:50:57 -0300

    Bee 1014

---
+++ src/Exercicios/bee1014.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1014 {
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int X = s.nextInt();
+        double Y = s.nextDouble();
+        double consumo = (double) X/Y;
+        System.out.printf("%.3f km/l\n",consumo);
+    }
+}
