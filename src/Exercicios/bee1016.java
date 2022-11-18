d5690caec4620421db00ca7b0c9ba97b1c94e6d5
João Vitor <joaovitor8832@gmail.com>
2022-11-18 01:30:56 -0300

    Bee 1016

---
+++ src/Exercicios/bee1016.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1016 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int distancia, tempo;
+        distancia = s.nextInt();
+        tempo = distancia*2;
+        System.out.println(tempo + " minutos");
+    }
+}
