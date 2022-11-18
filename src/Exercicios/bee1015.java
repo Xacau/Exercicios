6b531b3ac86396f4d03a64efae7720aa2553bc97
João Vitor <joaovitor8832@gmail.com>
2022-11-18 00:51:02 -0300

    Bee 1015

---
+++ src/Exercicios/bee1015.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1015 {
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        double x1,y1,x2,y2, distancia;
+        x1 = s.nextDouble();
+        y1 = s.nextDouble();
+        x2 = s.nextDouble();
+        y2 = s.nextDouble();
+        distancia = Math.sqrt((Math.pow((x2-x1),2))+Math.pow((y2-y1),2));
+        System.out.printf("%.4f\n", distancia);
+    }
+}
