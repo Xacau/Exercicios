bab863e777397962f6928c33d337b3b7dca3082d
João Vitor <joaovitor8832@gmail.com>
2022-11-18 01:31:00 -0300

    Bee 1017

---
+++ src/Exercicios/bee1017.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+import java.util.Scanner;
+public class bee1017 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int horas, velMedia, consumo;
+        double gasto;
+        consumo = 12;
+        horas = s.nextInt();
+        velMedia = s.nextInt();
+        gasto = (double) (horas*velMedia)/consumo;
+        System.out.printf("%.3f\n",gasto);
+    }
+}
