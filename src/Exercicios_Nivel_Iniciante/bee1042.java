b9a2155610bfa6f66c7c58c41ac510d15f2c65c0
João Vitor <joaovitor8832@gmail.com>
2022-11-18 21:48:42 -0300

    Bee 1042 - Nível Iniciante

---
+++ src/Exercicios_Nivel_Iniciante/bee1042.java
@@ -0,0 +1,41 @@
+package Exercicios_Nivel_Iniciante;
+
+import java.util.Scanner;
+public class bee1042 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int A,B,C;
+        A = s.nextInt();
+        B = s.nextInt();
+        C = s.nextInt();
+        if(A<B && A<C && B<C){
+            System.out.println(A);
+            System.out.println(B);
+            System.out.println(C);
+        }else if(A<B && A<C && C<B){
+            System.out.println(A);
+            System.out.println(C);
+            System.out.println(B);
+        }else if(B<A && B<C && A<C){
+            System.out.println(B);
+            System.out.println(A);
+            System.out.println(C);
+        }else if(B<A && B<C && C<A){
+            System.out.println(B);
+            System.out.println(C);
+            System.out.println(A);
+        }else if(C<A && C<B && A<B){
+            System.out.println(C);
+            System.out.println(A);
+            System.out.println(B);
+        }else if(C<A && C<B && B<A){
+            System.out.println(C);
+            System.out.println(B);
+            System.out.println(A);
+        }
+        System.out.println();
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    }
+}
