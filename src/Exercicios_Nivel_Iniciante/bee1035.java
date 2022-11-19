793781079c53ae68345021760ba4ecfe7c122659
João Vitor <joaovitor8832@gmail.com>
2022-11-18 21:46:15 -0300

    Bee 1035 - Nível Iniciante

---
+++ src/Exercicios_Nivel_Iniciante/bee1035.java
@@ -0,0 +1,18 @@
+package Exercicios_Nivel_Iniciante;
+
+import java.util.Scanner;
+public class bee1035 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int A,B,C,D;
+        A = s.nextInt();
+        B = s.nextInt();
+        C = s.nextInt();
+        D = s.nextInt();
+        if(B>C && D>A && (C+D)>(A+B) && C>0 && D>0 && A%2==0){
+            System.out.println("Valores aceitos");
+        }else{
+            System.out.println("Valores nao aceitos");
+        }
+    }
+}
