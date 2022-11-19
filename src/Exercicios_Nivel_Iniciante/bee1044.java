8521e1e63497d4d532ec4ad203bc25b85570fa7c
João Vitor <joaovitor8832@gmail.com>
2022-11-18 21:48:51 -0300

    Bee 1044 - Nível Iniciante

---
+++ src/Exercicios_Nivel_Iniciante/bee1044.java
@@ -0,0 +1,16 @@
+package Exercicios_Nivel_Iniciante;
+
+import java.util.Scanner;
+public class bee1044 {
+    public static Scanner s = new Scanner(System.in);
+    public static void main(String[] args){
+        int A,B;
+        A = s.nextInt();
+        B = s.nextInt();
+        if(A%B==0 || B%A==0){
+            System.out.println("Nao sao Multiplos");
+        }else{
+            System.out.println("Sao Multiplos");
+        }
+    }
+}
